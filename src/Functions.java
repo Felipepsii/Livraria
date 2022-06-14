8a58592ad46b325aaf9668860f183a60580868c1
Felipepsii <felipe811801@gmail.com>
2022-06-14 16:39:35 -0300

    1.1v

--- src/Functions.java
+++ src/Functions.java
@@ -36,6 +36,8 @@ public static void listar() throws SQLException {
 	System.out.println("-------------------------");
 	
 	}
+	
+	menu();
 }
 
 public static void incluir() throws SQLException {
@@ -71,172 +73,205 @@ public static void incluir() throws SQLException {
 	System.out.println("\n------------------------------------------");
 	
 	leitor.close();
-
+	
+	menu();
+	
 }
 
 public static void atualizar() throws SQLException {
-//1. Abrir conexï¿½o com banco de dados
-
-Scanner leitor = new Scanner(System.in);
-
-String url = ("jdbc:sqlite:C:\\Users\\jupit\\OneDrive\\Documentos\\Code\\Livraria\\db\\estoquedb.db");	
-Connection conexao = DriverManager.getConnection(url);
-
-
-//2. Seleção
-
-System.out.println("Qual o id do livro que você quer modificar?");
-
-int id = leitor.nextInt();
-
-System.out.println("O que você quer modificar?"
-		+ "\nSelecione "
-		+ "\n 1 - nome"
-		+ "\n 2 - autor"
-		+ "\n 3 - quantidade"
-		+ "\n 4 - valor");
-
-
-int mod = leitor.nextInt();	
-
-System.out.println("insira o novo valor");
-
-
-
-//3. Criar o comando e executar o SQL		
-Statement comando = conexao.createStatement();
-
-String queryUpdate = null;
-
-boolean  c = false;
-
-switch (mod) {
-case 1:
+	//1. Abrir conexï¿½o com banco de dados
 	
-	String val = leitor.nextLine();
-
-	queryUpdate = "update produto set name = '"+ val +"'"
-			+ "Where id = " + id;
-
-	comando.executeUpdate(queryUpdate);	
-	c = true;
-
+	Scanner leitor = new Scanner(System.in);
 	
+	String url = ("jdbc:sqlite:C:\\Users\\jupit\\OneDrive\\Documentos\\Code\\Livraria\\db\\estoquedb.db");	
+	Connection conexao = DriverManager.getConnection(url);
+	
+	
+	//2. Seleção
+	
+	System.out.println("Qual o id do livro que você quer modificar?");
+	
+	int id = leitor.nextInt();
+	
+	System.out.println("O que você quer modificar?"
+			+ "\nSelecione "
+			+ "\n 1 - nome"
+			+ "\n 2 - autor"
+			+ "\n 3 - quantidade"
+			+ "\n 4 - valor");
+	
+	
+	int mod = leitor.nextInt();	
+	
+	System.out.println("insira o novo valor");
+	
+	
+	
+	//3. Criar o comando e executar o SQL		
+	Statement comando = conexao.createStatement();
+	
+	String queryUpdate = null;
+	
+	boolean  c = false;
+	
+	switch (mod) {
+	case 1:
+		
+		String val = leitor.nextLine();
+	
+		queryUpdate = "update produto set name = '"+ val +"'"
+				+ "Where id = " + id;
+	
+		comando.executeUpdate(queryUpdate);	
+		c = true;
+	
+		
+		break;
+	case 2:
+		String val2 = leitor.next();
+	
+	
+		queryUpdate = "update produto set autor = '"+ val2 +"'"
+				+ "Where id = " + id;
+		
+		comando.executeUpdate(queryUpdate);	
+		c = true;
+		
+		break;
+	case 3:
+		
+		int val3 = leitor.nextInt();
+	
+		
+		queryUpdate = "update produto set quant = '"+ val3 +"'"
+				+ "Where id = " + id;
+		
+		comando.executeUpdate(queryUpdate);	
+		c = true;
+		
+		break;
+	case 4:
+		
+		double val4 = leitor.nextDouble();
+		
+		queryUpdate = "update produto set valor = '"+ val4 +"'"
+				+ "Where id = " + id;
+		comando.executeUpdate(queryUpdate);	
+		
+		c = true;
+		
+		break;
+	
+	default:
+	
+	c = false;
+	
+	
+	if (c == true) {
+	
+		System.out.println("------------------------------");
+		System.out.println("Atualizado com sucesso");
+		System.out.println("------------------------------");
+		
+	} else {
+	
+		System.out.println("Deu ruim");
+	}
+	
+	
+	leitor.close();
 	break;
-case 2:
-	String val2 = leitor.next();
-
-
-	queryUpdate = "update produto set autor = '"+ val2 +"'"
-			+ "Where id = " + id;
+	}
+	menu();
 	
-	comando.executeUpdate(queryUpdate);	
-	c = true;
-	
-	break;
-case 3:
-	
-	int val3 = leitor.nextInt();
-
-	
-	queryUpdate = "update produto set quant = '"+ val3 +"'"
-			+ "Where id = " + id;
-	
-	comando.executeUpdate(queryUpdate);	
-	c = true;
-	
-	break;
-case 4:
-	
-	double val4 = leitor.nextDouble();
-	
-	queryUpdate = "update produto set valor = '"+ val4 +"'"
-			+ "Where id = " + id;
-	comando.executeUpdate(queryUpdate);	
-	
-	c = true;
-	
-	break;
-
-default:
-
-c = false;
-
-
-if (c == true) {
-
-	System.out.println("------------------------------");
-	System.out.println("Atualizado com sucesso");
-	System.out.println("------------------------------");
-	
-} else {
-
-	System.out.println("Deu ruim");
-}
-
-leitor.close();
-}
-}
+	}
 
 public static void excluir() throws SQLException {
 
-Scanner leitor = new Scanner(System.in);
-//1. Abrir conexï¿½o com banco de dados
-String url = ("jdbc:sqlite:C:\\Users\\jupit\\OneDrive\\Documentos\\Code\\Livraria\\db\\estoquedb.db");
-Connection conexao = DriverManager.getConnection(url);
-
-
-System.out.println("Selecione pelo id qual livro você quer deletar");
-int id = leitor.nextInt();
-
-//2. Criar o comando e executar o SQL		
-Statement comando = conexao.createStatement();
-String queryUpdate = "DELETE FROM produto WHERE id = " + id;
-comando.executeUpdate(queryUpdate);	
-System.out.println("Livro do id " + id + " excluido com sucesso");
-leitor.close();
-
-}
+	Scanner leitor = new Scanner(System.in);
+	//1. Abrir conexï¿½o com banco de dados
+	String url = ("jdbc:sqlite:C:\\Users\\jupit\\OneDrive\\Documentos\\Code\\Livraria\\db\\estoquedb.db");
+	Connection conexao = DriverManager.getConnection(url);
+	
+	
+	System.out.println("Selecione pelo id qual livro você quer deletar");
+	int id = leitor.nextInt();
+	
+	//2. Criar o comando e executar o SQL		
+	Statement comando = conexao.createStatement();
+	String queryUpdate = "DELETE FROM produto WHERE id = " + id;
+	comando.executeUpdate(queryUpdate);	
+	System.out.println("Livro do id " + id + " excluido com sucesso");
+	leitor.close();
+	
+		menu();
+	}
 
 public static void console() throws SQLException {
 
-Scanner leitor = new Scanner(System.in);
-
-System.out.println("Selecione 1 para listar"
-		+ "\n\nSelecione 2 para incluir um novo livro"
-		+ "\n\nSelecione 3 para atualizar um livro existente"
-		+ "\n\nSelecione 4 para excluir um livro");
-
-
-int selecao = leitor.nextInt();
-
-switch (selecao) {
-case 1:
-	System.out.println("\nlista de livros:\n");
+	Scanner leitor = new Scanner(System.in);
 	
-	listar();
-	break;	
+	System.out.println("\nSelecione 1 para listar"
+			+ "\n\nSelecione 2 para incluir um novo livro"
+			+ "\n\nSelecione 3 para atualizar um livro existente"
+			+ "\n\nSelecione 4 para excluir um livro"
+			+ "\n\nSelecione 5 para sair");
+	
+	
+	int selecao = leitor.nextInt();
+	
+	switch (selecao) {
+	case 1:
+		System.out.println("\nlista de livros:\n");
+		
+		listar();
+		break;	
+	
+	case 2:
+		
+		incluir();	
+		break;
+	
+	case 3:
+		
+		atualizar();
+		break;
+		
+	case 4:
+		
+		excluir();
+		break;
+		
+	case 5:
+		
+		break;
+		
+	default:
+		System.out.println("Opção selecionada invalida");
+		console();
+		break;
+	}
+	
+	leitor.close();
+		
+		}
 
-case 2:
-	
-	incluir();	
-	break;
+public static void menu() throws SQLException {
 
-case 3:
 	
-	atualizar();
-	break;
 	
-case 4:
+	System.out.println("\n\nVoltando para o menu.....\n");
 	
-	excluir();
-	break;
 	
-default:
-	System.out.println("Opção selecionada invalida");
-	console();
+		console();
+
+	
 }
 
-leitor.close();
-}
+
+
+
+
+
+
 }
